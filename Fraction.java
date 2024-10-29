d17277a61c0d1ebbe1cf4aecf2455d7ac5d91851
Your Name <you@example.com>
2024-10-29 14:02:42 +0100

    initial commit

---
+++ Fraction.java
@@ -0,0 +1,36 @@
+public class Fraction {
+    static int numberOfFractions;
+    int numerator, denominator;
+    
+    // public Fraction(Fraction f) {
+    //     this(f.numerator, f.denominator);
+    // }
+
+    // public Fraction() {
+    //     this(0, 1);
+    // }
+
+    // public void setDenominator(int denominator) {
+    //     if (denominator == 0) {
+    //         throw new ArithmeticException("Division by zero");
+    //     } else {
+    //         this.denominator = denominator;
+    //     }
+    // }
+
+    // public void add(Fraction f) {
+    //     this.setNumerator(this.numerator * f.denominator + this.denominator * f.numerator);
+    //     this.setDenominator(this.denominator * f.denominator);
+    // }
+
+    // public static Fraction add(Fraction f, Fraction g) {
+    //     Fraction sum = new Fraction(f);
+    //     sum.add(g);
+    //     return sum;
+    // }
+    
+    // public boolean equals(Fraction f) {
+    //     return this.numerator * f.denominator == this.denominator * f.numerator;
+    // }
+
+}
